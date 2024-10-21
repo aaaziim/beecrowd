d83e90e9042e5d3a6f62705a13fe985e2837fe9f
aaaziim <azim210215@gmail.com>
2024-10-21 23:09:03 +0600

    Extremely Basic Solved

---
+++ Extremely Basic/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, X;
+
+    // Reading two integers
+    scanf("%d %d", &A, &B);
+
+    // Calculating the sum and storing it in X
+    X = A + B;
+
+    // Printing the result in the required format
+    printf("X = %d\n", X);
+
+    return 0;
+}
