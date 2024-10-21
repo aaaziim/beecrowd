ed43d9cf6421e831cd88474057898f3023f23e16
aaaziim <azim210215@gmail.com>
2024-10-21 23:11:35 +0600

    Simple Product Solved

---
+++ Simple Product/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, PROD;
+
+    // Reading two integers
+    scanf("%d %d", &A, &B);
+
+    // Calculating the product and storing it in PROD
+    PROD = A * B;
+
+    // Printing the result in the required format
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
