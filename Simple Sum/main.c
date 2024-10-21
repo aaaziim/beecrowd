c78032d0ff1429f573e851521c9186023fd1256a
aaaziim <azim210215@gmail.com>
2024-10-21 23:12:19 +0600

    Simple Sum Solved

---
+++ Simple Sum/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, SOMA;
+
+    // Reading two integers
+    scanf("%d %d", &A, &B);
+
+    // Calculating the sum and storing it in SOMA
+    SOMA = A + B;
+
+    // Printing the result in the required format
+    printf("SOMA = %d\n", SOMA);
+
+    return 0;
+}
