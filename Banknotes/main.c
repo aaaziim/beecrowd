87acd5c37a4032d689701e457b5763306d83181f
aaaziim <azim210215@gmail.com>
2024-10-23 23:49:16 +0600

    Banknotes Fixed

--- Banknotes/main.c
+++ Banknotes/main.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
 
 int main() {
-    int N;
-    scanf("%d", &N); // Read the integer value
+    int N, remainder;
+    int notes100, notes50, notes20, notes10, notes5, notes2, notes1;
 
-    // Array of banknote denominations
-    int banknotes[] = {100, 50, 20, 10, 5, 2, 1};
-    int quantity[7] = {0}; // To hold the quantity of each banknote
+    // Reading the input value
+    scanf("%d", &N);
 
-    // Calculate the quantity of each banknote
-    for (int i = 0; i < 7; i++) {
-        quantity[i] = N / banknotes[i]; // Number of banknotes of this denomination
-        N %= banknotes[i]; // Remaining amount after using this denomination
-    }
+    // Display the input value
+    printf("%d\n", N);
 
-    // Print the result
-    printf("%d\n", N + (N > 0 ? banknotes[0] : 0)); // Print the original value
+    // Calculating the number of each banknote
+    notes100 = N / 100;
+    remainder = N % 100;
 
-    for (int i = 0; i < 7; i++) {
-        printf("%d nota(s) de R$ %.2f\n", quantity[i], (float)banknotes[i]);
-    }
+    notes50 = remainder / 50;
+    remainder = remainder % 50;
+
+    notes20 = remainder / 20;
+    remainder = remainder % 20;
+
+    notes10 = remainder / 10;
+    remainder = remainder % 10;
+
+    notes5 = remainder / 5;
+    remainder = remainder % 5;
+
+    notes2 = remainder / 2;
+    remainder = remainder % 2;
+
+    notes1 = remainder;
+
+    // Printing the number of each banknote
+    printf("%d nota(s) de R$ 100,00\n", notes100);
+    printf("%d nota(s) de R$ 50,00\n", notes50);
+    printf("%d nota(s) de R$ 20,00\n", notes20);
+    printf("%d nota(s) de R$ 10,00\n", notes10);
+    printf("%d nota(s) de R$ 5,00\n", notes5);
+    printf("%d nota(s) de R$ 2,00\n", notes2);
+    printf("%d nota(s) de R$ 1,00\n", notes1);
 
     return 0;
 }
